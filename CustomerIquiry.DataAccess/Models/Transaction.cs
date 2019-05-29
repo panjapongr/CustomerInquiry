f76084555c24b216f740d94c9d697db37505a42f
Panjapong Ruengseree <panjapong.ruengseree@accenture.com>
2019-05-29 15:31:16 +0700

    Create model.

---
+++ CustomerIquiry.DataAccess/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerIquiry.DataAccess.Models
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public DateTime Date { get; set; }
+        public Decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
