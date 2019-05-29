258715a98a4c22f8bc711ae117bd99d97a641896
Panjapong Ruengseree <panjapong.ruengseree@accenture.com>
2019-05-29 15:54:45 +0700

    Create Customer API.

---
+++ CustomerInquiry.Api/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using CustomerIquiry.DataAccess;
+using CustomerIquiry.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CustomerInquiry.Api.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public Customer Get(int customerID, string email)
+        {
+            return new CustomerContext().Customer
+                            .Where(c => customerID == c.CustomerID && email == c.Email)
+                            .SingleOrDefault();
+        }
+    }
+}
